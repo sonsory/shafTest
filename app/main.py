f8d4e97ea5bc45cb36805ff09c52040f8c8fa06b
namhyun Kim <sonsory@gmail.com>
2023-09-12 11:58:08 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent
 app = FastAPI()
 
 
-templates = Jinja2Templates(directory=BASE_DIR / "templates")
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 
 def read_img(img):
